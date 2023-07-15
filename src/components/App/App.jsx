ba7d2ed079f6b3b5e04cfe1739928e44cf687cf6
Sam McCauley <sammccauley34@gmail.com>
2023-07-15 15:05:26 -0500

    Created FeelingForm comp. and added boilerplate

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Feedback!</h1>
+          <h4>Don't forget it!</h4>
+        </header>
+        <nav>
+          <li>
+            <Link>FeelingForm</Link>
+          </li>
+          <li>
+            <Link>UnderstandForm</Link>
+          </li>
+          <li>
+            <Link>X</Link>
+          </li>
+        </nav>
+
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+import React, { useState } from "react";
+
+function FeelingForm() {
+
+    return (
+        <>
+        <h2>How are you feeling today?</h2>
+        <input type="number" placeholder="1-5"></input>
+        <button>NEXT</button>
+        </>
+    )
+}
+
+export default FeelingForm;
