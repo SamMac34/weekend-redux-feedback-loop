e00ad8efb891d9afc509029c606421d39d61eda8
Sam McCauley <sammccauley34@gmail.com>
2023-07-15 18:15:48 -0500

    Added logic to update first key in response reducer

--- src/components/FeelingForm/FeelingForm.jsx
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -1,13 +1,28 @@
-// import { useState } from "react";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 function FeelingForm() {
+    const dispatch = useDispatch();
+    let [feeling, setFeeling] = useState(0);
+
+    const handleFeeling = (event) => {
+        console.log('in handleFeeling, e.t.v is:', event.target.value);
+        setFeeling(event.target.value)
+    };
+
+    const addFeeling = () => {
+        console.log('in addFeeling');
+        dispatch({
+            type: 'ADD_FEELING',
+            payload: feeling,
+        })
+    };
 
     return (
         <>
         <h2>How are you feeling today?</h2>
-        <input type="number" placeholder="1-5"></input>
-        <button>NEXT</button>
+        <input onChange={handleFeeling} type="number" placeholder="1-5"></input>
+        <button onClick={addFeeling}>NEXT</button>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -8,15 +8,28 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from "react-redux";
 import logger from "redux-logger";
 
-// Response reducer
-const responses = (state = {}, action) => {
 
+// Response reducer
+// { feeling: , understanding: , support: , comments: }
+const responseReducer = (state = {
+    feeling: '', 
+    understanding: '',
+    support: '',
+    comments: ''},
+    action) => {
+    if (action.type === 'ADD_FEELING') {
+        console.log('In responses reducer:', action.payload, state)
+        return { ...state, feeling: action.payload }; 
+    } else if (action.type === 'ADD_UNDERSTAND'){
+        return { ...state, understanding: action.payload }
+    }
     return state;
 }
 
+// Store
 const store = createStore(
     combineReducers({
-        responses
+        responseReducer
     }),
     applyMiddleware(logger)
 );
