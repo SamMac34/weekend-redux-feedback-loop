a808e5b382f04d75fc9bba79e6d135ce365390d7
Sam McCauley <sammccauley34@gmail.com>
2023-07-15 15:30:20 -0500

    Set up responses reducer and store in index

--- src/index.js
+++ src/index.js
@@ -3,9 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+
+// Response reducer
+const responses = (state = {}, action) => {
+
+    return state;
+}
+
+const store = createStore(
+    combineReducers({
+        responses
+    }),
+    applyMiddleware(logger)
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+   
     <React.StrictMode>
+        <Provider store={store}>
         <App />
+        </Provider>
     </React.StrictMode>
 );
